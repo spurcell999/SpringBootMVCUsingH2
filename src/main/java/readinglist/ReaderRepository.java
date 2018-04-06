5f85f5d27946f7414f4ad67da6881607a436b24d
glob <glob@aol>
2018-04-06 15:47:38 -0500

    Initial

---
+++ src/main/java/readinglist/ReaderRepository.java
@@ -0,0 +1,10 @@
+package readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by e026459 on 12/18/2017.
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+
+}
